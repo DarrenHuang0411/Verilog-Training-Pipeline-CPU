6da30feda6bb2c56008ee4b3784f1451c4ce9e5e
Darren <ades96063@gmail.com>
2024-08-21 21:42:23 +0800

    define.sv add

--- define/def.sv
+++ define/def.sv
@@ -1,1 +1,11 @@
+`ifndef _PARAMETER_SETTING_
+`define _PARAMETER_SETTING_
 
+//DATA Parameter
+    `define DATA_WIDTH      32
+
+    `define OP_CODE         7
+    `define FUNCTION_3      3
+    `define FUNCTION_7      7
+
+`endif
