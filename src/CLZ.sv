172f44943b05d503331735a753294d603f9059ed
DarrenHuang0411 <ades96063@gmail.com>
2024-09-27 12:02:55 +0800

    CPU_v2.3_beta1

--- src/CLZ.sv
+++ src/CLZ.sv
@@ -2,7 +2,7 @@
 
 module CLZ (
     input       [`DATA_WIDTH -1:0]  significand_in,
-    output      [ 5:0]  CLZ_result
+    output      [ 5:0]              CLZ_result
 );
 
     significand_s16
@@ -13,14 +13,14 @@ module CLZ (
       
     count_s16
 
-    assign  CLZ_result  =   (count_s16 + )
+    assign  CLZ_result  =   (count_s16 + ) 
 
     always_comb begin
         count_s16   =   0;
         count_s8    =   0;
         significand_s16  =   {input[15:0]}
 
-        if ((input && 0xff_ff00) == 0) begin
+        if ((significand_in && 0xff_ff00) == 0) begin
            count_s16        =   16;
            if()
         end
