6b338311b992ade97db29c68a14c24bde1128a23
Darren <ades96063@gmail.com>
2024-09-27 23:58:05 +0800

    CPU_v2.3
    floating point sub finish
    next: CSR

--- src/CLZ.sv
+++ src/CLZ.sv
@@ -5,29 +5,70 @@ module CLZ (
     output      [ 5:0]              CLZ_result
 );
 
-    significand_s16
-    significand_s16_8
-    significand_s16_4
-    significand_s16_2  
+    reg [`DATA_WIDTH -1:0] significand_s1;
+    reg [`DATA_WIDTH -1:0] significand_s2;
+    reg [`DATA_WIDTH -1:0] significand_s3;
+    reg [`DATA_WIDTH -1:0] significand_s4; 
     
-      
-    count_s16
-
-    assign  CLZ_result  =   (count_s16 + ) 
+    reg [5:0]              count_s16;
+    reg [5:0]              count_s8;
+    reg [5:0]              count_s4;
+    reg [5:0]              count_s2;
+    reg [5:0]              count_s1;
+  
+    always_comb begin
+        if (!(significand_in & 32'hffff_0000)) begin
+           count_s16        =   16;
+           significand_s1   =   {significand_in[15:0] ,16'd0};
+        end
+        else begin
+            count_s16       =   0;
+            significand_s1  =   significand_in;
+        end
+    end
 
     always_comb begin
-        count_s16   =   0;
-        count_s8    =   0;
-        significand_s16  =   {input[15:0]}
-
-        if ((significand_in && 0xff_ff00) == 0) begin
-           count_s16        =   16;
-           if()
+        if (!(significand_s1 & 32'hff00_0000)) begin
+           count_s8         =    8;
+           significand_s2   =   {significand_s1[23:0] ,8'd0};
         end
-        else if((input && 0xff_0000) == 0) begin
-            count_s8        =   8;
-        end 
-
+        else begin
+            count_s8        =   0;
+            significand_s2  =   significand_s1;
+        end
     end
+
+    always_comb begin
+        if (!(significand_s2 & 32'hf000_0000)) begin
+           count_s4         =    4;
+           significand_s3   =   {significand_s2[27:0] ,4'd0};
+        end
+        else begin
+            count_s4        =   0;
+            significand_s3  =   significand_s2;
+        end
+    end
+
+    always_comb begin
+        if (!(significand_s3 & 32'hc000_0000)) begin
+           count_s2         =    2;
+           significand_s4   =   {significand_s3[29:0] ,2'd0};
+        end
+        else begin
+            count_s2        =   0;
+            significand_s4  =   significand_s3;
+        end
+    end
+
+    always_comb begin
+        if (!(significand_s4 & 32'h8000_0000)) begin
+            count_s1         =   1;
+        end
+        else begin
+            count_s1         =   0;
+        end
+    end
+
+    assign  CLZ_result  =   (count_s16 + count_s8) + (count_s4 + count_s2) + count_s1;
 
 endmodule
--- src/EXE_FP_ALU.sv
+++ src/EXE_FP_ALU.sv
@@ -1,7 +1,9 @@
 //------------------------- File Info -------------------------//
 
 //------------------------ Environment ------------------------//
-`define minus_abs(a,b) ((a>=b) ? a-b : b-a)
+    `include "CLZ.sv"
+    `define minus_abs(a,b) ((a>=b) ? a-b : b-a)
+
 
 module EXE_FP_ALU (
 //Ctrl
@@ -14,6 +16,7 @@ module EXE_FP_ALU (
     //output  reg  zeroflag
 );
 //------------------------- parameter -------------------------//    
+    wire                        compare_flag;
     reg     [`DATA_WIDTH -1:0]  FPU_rs1;
     reg     [`DATA_WIDTH -1:0]  FPU_rs2;    
     wire    [`DATA_WIDTH -1:0]  significand_rs1;
@@ -29,14 +32,20 @@ module EXE_FP_ALU (
     wire    [`DATA_WIDTH :0]    significand_add;
     reg     [`EXP -1:0]         exp_add; 
     reg     [`FRACTION -1:0]    fract_add;
+    wire    [`DATA_WIDTH -1:0]  significand_sub;
+    wire    [5:0]               sub_shift;
+    wire    [`EXP -1:0]         exp_sub; 
+    wire    [`DATA_WIDTH -1:0]  sub_normalize_temp;
+    reg     [`FRACTION -1:0]    fract_sub;
 
     //------------------Float Point ---------------------------// 
     localparam  [4:0]   ALU_FP_add  =   5'd22,
                         ALU_FP_sub  =   5'd23;                        
 
 //------------------------- Basic -------------------------//
+    assign  compare_flag    =   rs1[30:0] >= rs2[30:0];
     always_comb begin
-        if (rs1[30:0] >= rs2[30:0]) begin
+        if (compare_flag) begin
             FPU_rs1 = rs1;
             FPU_rs2 = rs2;            
         end 
@@ -51,9 +60,9 @@ module EXE_FP_ALU (
     assign  shift               =   `minus_abs(rs1[30:23],rs2[30:23]);
     assign  significand_rs2_s   =   significand_rs2 >> shift;
 
-    assign  signed_out          =   FPU_rs1[31];
-    assign  exp_out             =   (ALU_real_type) ? exp_add   : 8'b0;//exp_sub;
-    assign  fract_out           =   (ALU_real_type) ? fract_add : 23'b0;//fract_sub; 
+    assign  signed_out          =   ((FP_ALU_ctrl == ALU_FP_sub) && !compare_flag) ? !(FPU_rs1[31]): FPU_rs1[31];
+    assign  exp_out             =   (ALU_real_type) ? exp_add   : exp_sub;//exp_sub;
+    assign  fract_out           =   (ALU_real_type) ? fract_add : fract_sub;//fract_sub; 
     assign  ALU_FP_out          =   {signed_out,exp_out, fract_out};
 
     // //Exception flag sets 1 if either one of the exponent is 255.
@@ -73,25 +82,64 @@ module EXE_FP_ALU (
     always_comb begin
         if(significand_add[32]) begin
             exp_add     =   FPU_rs1[30:23] + 1'b1;
-            fract_add   =   significand_add[31:9];
+
+            if(significand_add[8:7] == 2'b11) begin
+                fract_add   =   significand_add[31:9] + 1;
+            end
+            else if (significand_add[8:7] == 2'b10) begin
+            //sticky bit + LSB(round to nearest even)
+                if((|significand_add[6:0]) || significand_add[9])
+                    fract_add   =   significand_add[31:9] + 1;    
+                else
+                    fract_add   =   significand_add[31:9];
+            end
+            else begin
+                fract_add   =   significand_add[31:9];
+            end
         end
+
         else begin
             exp_add     =   FPU_rs1[30:23];
-            fract_add   =   significand_add[30:8];            
+
+            if(significand_add[7:6] == 2'b11) begin
+                fract_add   =   significand_add[30:8] + 1;
+            end
+            else if (significand_add[7:6] == 2'b10) begin
+            //sticky bit + LSB(round to nearest even)
+                if((|significand_add[5:0]) || significand_add[8])
+                    fract_add   =   significand_add[30:8] + 1;    
+                else
+                    fract_add   =   significand_add[30:8];
+            end
+            else begin
+                fract_add   =   significand_add[30:8];
+            end        
         end
     end
 
 //-------------------------- Substraction --------------------------------//
     assign  significand_sub    =    significand_rs1 - significand_rs2_s; //32 bit
-    
+    CLZ CLZ_inst(
+        .significand_in(significand_sub),
+        .CLZ_result(sub_shift)
+    );
+    assign  exp_sub             =   FPU_rs1[30:23] - sub_shift;
+    assign  sub_normalize_temp  =   significand_sub << sub_shift;
+
     always_comb begin
-        if(significand_[32]) begin
-            exp_add     =   FPU_rs1[30:23] + 1'b1;
-            fract_add   =   significand_add[31:9];
+      //Guard_round bit 
+        if(sub_normalize_temp[7:6] == 2'b11) begin
+            fract_sub   =   sub_normalize_temp[30:8] + 1;
+        end
+        else if (sub_normalize_temp[7:6] == 2'b10) begin
+          //sticky bit + LSB(round to nearest even)
+            if((|sub_normalize_temp[5:0]) || sub_normalize_temp[8])
+                fract_sub   =   sub_normalize_temp[30:8] + 1;
+            else
+                fract_sub   =   sub_normalize_temp[30:8];
         end
         else begin
-            exp_add     =   FPU_rs1[30:23];
-            fract_add   =   significand_add[30:8];            
+            fract_sub   =   sub_normalize_temp[30:8];
         end
     end
 
